08a25861e6c8424b0e3e55ccbbc28967615d5d16
Taksh Soni <sonitaksh@gmail.com>
2019-10-22 15:00:44 +0530

    Added classes

---
+++ app/src/main/java/com/example/healthcare/classes/BMI_Diet_plan.java
@@ -0,0 +1,18 @@
+package com.example.healthcare.classes;
+
+public class BMI_Diet_plan {
+    float BMI;
+    int expectedSteps;
+    int[] timeList;
+    String[] foodList;
+    String exercise;
+
+    void modifies(){
+
+    }
+
+    void suggests(){
+
+    }
+
+}
